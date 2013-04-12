cc0e004174b3af5ece34bed57b4a17508471f473
Simon Urli <simon.urli@gmail.com>
2013-04-12 10:20:57 +0200

    Add a new test file for issue refs #20

---
+++ FAMILIAR/src/fr/unice/polytech/modalis/familiar/test/FMLConfigurationFMWithMultipleGroups.java
@@ -0,0 +1,35 @@
+package fr.unice.polytech.modalis.familiar.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.xtext.example.mydsl.fML.OpSelection;
+
+import fr.unice.polytech.modalis.familiar.parser.ConfigurationVariableFactory;
+import fr.unice.polytech.modalis.familiar.variable.ConfigurationVariable;
+import fr.unice.polytech.modalis.familiar.variable.FeatureModelVariable;
+
+public class FMLConfigurationFMWithMultipleGroups extends FMLTest {
+
+	@Test
+	public void testConfigurationWithMultipleGroupsSelectsByDefaultMandatoryFeaturesWithFeatureIDE() throws Exception {
+		FeatureModelVariable fm1 = FM ("fm1", "FM (A: B C D; B: (X|Y)+ [Z]; C: (E|F|G); D: (I|J) K;)");
+		assertNotNull(fm1.mkSPLOTFla(fm1.toSPLOT(), _builder));
+		
+		ConfigurationVariable c1 =ConfigurationVariableFactory.INSTANCE.mkFeatureIDE(fm1, "c1");
+		assertEquals (0, c1.getDeselected().size());
+		assertEquals (4, c1.getSelected().size());
+	}
+	
+	@Test
+	public void testConfigurationWithMultipleGroupsSelectsByDefaultMandatoryFeaturesWithSPLOT() throws Exception {
+		FeatureModelVariable fm1 = FM ("fm1", "FM (A: B C D; B: (X|Y)+ [Z]; C: (E|F|G); D: (I|J) K;)");
+		assertNotNull(fm1.mkSPLOTFla(fm1.toSPLOT(), _builder));
+		
+		ConfigurationVariable c1 =ConfigurationVariableFactory.INSTANCE.mkSPLOT(fm1, "c1"); 
+		assertEquals (0, c1.getDeselected().size());
+		assertEquals (4, c1.getSelected().size());
+	}
+}
