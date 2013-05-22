848404fe001d792852b5e49a189768911d55b752
Simon Urli <simon.urli@gmail.com>
2013-05-22 15:00:48 -0700

    Add a unit test for issue #26

---
+++ FAMILIAR/src/fr/unice/polytech/modalis/familiar/test/FMMergeSunionKeepSemanticInConfiguration.java
@@ -0,0 +1,34 @@
+package fr.unice.polytech.modalis.familiar.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xtext.example.mydsl.fML.OpSelection;
+
+import fr.unice.polytech.modalis.familiar.interpreter.VariableNotExistingException;
+import fr.unice.polytech.modalis.familiar.parser.ConfigurationVariableFactory;
+import fr.unice.polytech.modalis.familiar.parser.VariableAmbigousConflictException;
+import fr.unice.polytech.modalis.familiar.variable.ConfigurationVariable;
+import fr.unice.polytech.modalis.familiar.variable.FeatureModelVariable;
+
+public class FMMergeSunionKeepSemanticInConfiguration extends FMLTest {
+
+	private FeatureModelVariable fm1,fm2,fm3;
+	
+	
+	@Test
+	public void testMergeAndConfigurationS1() throws Exception {
+		this.fm1 = FM("s1","Source: TI Criteria; TI: Forecast; Forecast: Weather2; Criteria: Date Location;");
+		this.fm2 = FM("s2","Source: TI Criteria; TI: Ephem; Criteria: Date; Date: Beginning; Beginning: Now;");
+		this.fm3 = FM("s3","Source: TI Criteria; TI: Calendar; Criteria: Location Duration Date;");
+		
+		_shell.parse("fmMerge = merge sunion s*");
+		FeatureModelVariable fmMerge = (FeatureModelVariable) _environment.getVariable("fmMerge");
+		ConfigurationVariable c1 =ConfigurationVariableFactory.INSTANCE.mkSPLOT(fmMerge, "cMerge");
+		c1.applySelection("Weather2", OpSelection.SELECT);
+		assertTrue(c1.getSelected().contains("Date"));
+		assertTrue(c1.getSelected().contains("Location"));
+		assertFalse(c1.getSelected().contains("Duration"));
+	}
+}
