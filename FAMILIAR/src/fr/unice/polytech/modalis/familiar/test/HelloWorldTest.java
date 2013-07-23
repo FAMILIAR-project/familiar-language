a01fc9dfa6aef6471e1ced18c95d525f5ff44051
Mathieu Acher <mathieu.acher@irisa.fr>
2013-07-23 12:03:31 +0200

    HW test (configuration)

--- FAMILIAR/src/fr/unice/polytech/modalis/familiar/test/HelloWorldTest.java
+++ FAMILIAR/src/fr/unice/polytech/modalis/familiar/test/HelloWorldTest.java
@@ -8,7 +8,10 @@ import static org.junit.Assert.assertEquals;
 import java.util.Collection;
 
 import org.junit.Test;
+import org.xtext.example.mydsl.fML.OpSelection;
 
+import fr.unice.polytech.modalis.familiar.parser.ConfigurationVariableFactory;
+import fr.unice.polytech.modalis.familiar.variable.ConfigurationVariable;
 import fr.unice.polytech.modalis.familiar.variable.FeatureModelVariable;
 import fr.unice.polytech.modalis.familiar.variable.SetVariable;
 import gsd.graph.ImplicationGraph;
@@ -33,6 +36,19 @@ public class HelloWorldTest extends FMLTest {
 		for (SimpleEdge e : e1) {
 			System.err.println("" + big1.getEdgeSource(e) + " => " + big1.getEdgeTarget(e));
 		}
+					
+	}
+	
+	@Test
+	public void test2() throws Exception {
+		
+		FeatureModelVariable fm1 = FM("fm1", "A : B [C] D ; D : (E|F|G) ; C -> E ; ");
+		ConfigurationVariable cf1 = ConfigurationVariableFactory.INSTANCE.mkBDD(fm1, "cf1");
+		
+		cf1.applySelection("C", OpSelection.SELECT);
+		System.err.println("" + cf1.getSelected() + " " + cf1.getDeselected()); 
+		
+		
 					
 	}
 	
