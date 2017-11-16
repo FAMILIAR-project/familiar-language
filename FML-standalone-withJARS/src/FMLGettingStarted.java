c8f3b669afad1dba23271046fbc809c1e4720625
Mathieu Acher <mathieu.acher@irisa.fr>
2017-11-16 14:32:41 +0100

    minimal

--- FML-standalone-withJARS/src/FMLGettingStarted.java
+++ FML-standalone-withJARS/src/FMLGettingStarted.java
@@ -16,13 +16,12 @@ public class FMLGettingStarted extends FMLTest {
     @Test
     public void testHelloWorld() throws Exception {
         FeatureModelVariable fmv = FM ("fm1", "FM (A : [B] [C] ;)");
-        FeatureModelLoader loader = new FeatureModelLoader(_shell, _builder);
+        
         assertEquals(4.0, fmv.counting(), 0.0);
 
         FeatureModelVariable fmv2 = new FeatureModelVariable ("fm2", FMBuilder.getInternalFM("A : [B] [C] ;"));
         assertEquals(Comparison.REFACTORING, fmv2.compare(fmv));
         
-        // assertEquals(201, loader.getAllSPLOTFeatureModels().size());
     }
 }
 
