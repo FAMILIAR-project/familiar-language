e88a30fedd99027984c693e18f886de2aee7551a
Mathieu Acher <mathieu.acher@irisa.fr>
2017-11-16 13:39:18 +0100

    Getting started with Maven or raw jars

---
+++ FML-gettingstarted/src/test/java/FMLHelloWorld.java
@@ -0,0 +1,25 @@
+import fr.familiar.FMLTest;
+import fr.familiar.FeatureModelLoader;
+import fr.familiar.parser.FMBuilder;
+import fr.familiar.variable.Comparison;
+import fr.familiar.variable.FeatureModelVariable;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by macher1 on 16/11/2017.
+ */
+public class FMLHelloWorld extends FMLTest {
+
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        FeatureModelVariable fmv = FM ("fm1", "FM (A : [B] [C] ;)");
+        FeatureModelLoader loader = new FeatureModelLoader(_shell, _builder);
+        assertEquals(4.0, fmv.counting(), 0.0);
+
+        FeatureModelVariable fmv2 = new FeatureModelVariable ("fm2", FMBuilder.getInternalFM("A : [B] [C] ;"));
+        assertEquals(Comparison.REFACTORING, fmv2.compare(fmv));
+    }
+}
